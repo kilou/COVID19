49673dfc7da4afa66d0e1999c2abac80464cf364
Pickoffs <sui.bastien@gmail.com>
2020-04-29 10:57:20 +0200

    Update autorun.R

--- autorun.R
+++ autorun.R
@@ -1,4 +1,25 @@
 # run local github shiny app
+rm(list=ls())
+
+reqpack <- function(X) {
+  pak_failed <- which(!unlist(lapply(X, require, character.only = TRUE)))
+  if(length(pak_failed)>=1){
+    install.packages(X[pak_failed], repos = "https://stat.ethz.ch/CRAN/")
+    lapply(X[pak_failed], require, character.only = TRUE)
+  }
+}
+pkg_list <- c("dplyr",
+              "ggplot2",
+              "readxl",
+              "shiny",
+              "shinybusy",
+              "snowfall",
+              "tippy",
+              "rhandsontable",
+              "plotly",
+              "writexl")
+reqpack(pkg_list)
+
 
 #downloading the files
 download.file(url = 'https://github.com/kilou/COVID19/archive/master.zip',destfile = 'covid_app.zip')
@@ -10,4 +31,5 @@ unzip(zipfile = './covid_app.zip',overwrite = T)
 
 setwd("./COVID19-master")
 source("app.R")
+setwd('../')
 shinyApp(ui, server)
