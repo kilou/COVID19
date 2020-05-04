45e8efbc8d3d843218d9fd3fabbeb883771ea9ce
Aziz Chaouch <aziz.chaouch@gmail.com>
2020-05-04 16:29:31 +0200

    Modified survival.r

--- surv/survival.r
+++ surv/survival.r
@@ -5,6 +5,10 @@ library(readxl)
 library(writexl)
 library(survival)
 
+# Time (for predictions)
+Time <- c(0.001,1:60)
+npred <- length(Time)
+
 # Load data
 raw <- as.data.frame(read_xlsx("data/20.04.17 - Données REDCap hôpitaux anonymisés.xlsx",sheet=1))
 age <- raw[,"age"]
@@ -16,6 +20,10 @@ hos_out <- as.Date(raw[,"sortie_hopital"],"%Y/%m/%d")
 dead <- raw[,"deces"]
 exit <- (!is.na(hos_out))*1
 
+# Calculate lag
+lag <- as.numeric(icu_in-hos_in)
+fit.lag <- fit.nb(lag)
+
 # Calculate total LOS
 los <- as.numeric(hos_out-hos_in)
 cens <- (is.na(hos_out))*1
@@ -23,7 +31,7 @@ los[cens==1] <- as.numeric(max(hos_in,na.rm=T)-hos_in[cens==1])
 los[los==0] <- 0.001 # to avoid issues with los=0
 
 # Distribution of age categories and proportion of females in each age category
-age.breaks <- c(0,15,30,45,60,75,90,105)
+age.breaks <- c(0,64,74,104)
 ncat <- length(age.breaks)-1
 agecat <- cut(age,breaks=age.breaks,include.lowest=TRUE)
 page <- round(table(agecat)/nrow(raw),3); page[ncat] <- 1-sum(page[1:(ncat-1)])
@@ -31,11 +39,11 @@ pfem <- round(table(agecat[sex=="F"])/table(agecat),3)
 
 # Plot histogram of age distribution for each sex
 pop <- rpop(n=1e06,breaks=age.breaks,page,pfem)
-xM <- pop$age[pop$sex=="M"]; attr(xM,"breaks") <- age.breaks
-xF <- pop$age[pop$sex=="F"]; attr(xF,"breaks") <- age.breaks
-par(mfrow=c(1,2),mar=c(3,3,2,0.5),mgp=c(1.8,0.6,0))
-histo(xM); title("Males")
-histo(xF); title("Females")
+# xM <- pop$age[pop$sex=="M"]; attr(xM,"breaks") <- age.breaks
+# xF <- pop$age[pop$sex=="F"]; attr(xF,"breaks") <- age.breaks
+# par(mfrow=c(1,2),mar=c(3,3,2,0.5),mgp=c(1.8,0.6,0))
+# histo(xM); title("Males")
+# histo(xF); title("Females")
 
 ###############################################################################
 # MODEL PROPORTION OF PATIENTS REQUIRING IC (BY AGE AND SEX)
@@ -49,68 +57,21 @@ ic[pos0] <- 0
 ic[pos1] <- 1
 
 # Initial model
-fm <- glm(ic~age+sex,family="binomial")
-AIC(fm)
-best.FP(fm,xform=~age,degree=1)$aic
-best.FP(fm,xform=~age,degree=2)$aic
-best.FP(fm,xform=~age,degree=2)$power
+fm20 <- glm(ic~cut(age,breaks=c(0,64,74,104),include.lowest=T)+sex,family="binomial"); aic(fm20)
+fm21 <- glm(ic~cut(age,breaks=c(0,64,74,104),include.lowest=T)*sex,family="binomial"); aic(fm21)
+summary(fm20)
 
 # Best icp model
 #******************************************************
-formula.icp <- ic~FP(age,c(3,3),shift=1,scale=100)+sex
+formula.icp <- ic~cut(age,breaks=c(0,64,74,104),include.lowest=T)+sex
 #******************************************************
+col.icp <- c("green","orange","red")
+mid.icp <- tapply(age,cut(age,breaks=c(0,64,74,104),include.lowest=T),mean)
 
-fm <- glm(formula.icp,family="binomial")
-X <- model.matrix(xform(formula.icp))
-
-# Multiple imputations for IC status of hospitalized patients but not yet in ICU
-niter <- 500         # nb iterations
-M <- 20              # nb imputed datasets
-nimp <- length(pos2) # nb of imputed data
-npar <- ncol(X)      # nb parameters in logistic model
-beta.trace <- matrix(nrow=niter,ncol=npar)
-vcov.trace <- rep(list(NULL),niter)
-
-icp.imp <- rep(0,nimp) # individual probability to require IC
-for(i in 1:niter){
-  beta.tmp <- matrix(nrow=M,ncol=npar)
-  vcov.tmp <- rep(list(NULL),M)
-  for(m in 1:M){
-    ic[pos2] <- sapply(icp.imp,rbinom,n=1,size=1)
-    fm.tmp <- update(fm)
-    beta.tmp[m,] <- coef(fm.tmp)
-    vcov.tmp[[m]] <- vcov(fm.tmp)
-  }
-  
-  # Pooled estimates
-  beta.trace[i,] <- apply(beta.tmp,2,mean)
-  
-  # Rubin rules
-  Vw <- Reduce("+",vcov.tmp)/M
-  Vb <- var(beta.tmp)
-  vcov.trace[[i]] <- Vw+(1+1/M)*Vb
-  
-  # Update IC requirement probability for patients without IC status
-  icp.imp <- expit(as.numeric(X[pos2,]%*%beta.trace[i,]))
-}  
-
-# Convergence plots
-par(mfrow=c(2,2),mar=c(3,3,2,0.5),mgp=c(1.8,0.6,0))
-for(k in 1:npar){
-  plot(beta.trace[,k],type="l")
-}
-
-# Average parameters over last 100 iterations
-last <- 100
-beta.last <- beta.trace[(niter-last+1):niter,]
-vcov.last <- vcov.trace[(niter-last+1):niter]
-beta.icp <- apply(beta.last,2,mean)
-Vw <- Reduce("+",vcov.last)/last
-Vb <- var(beta.last)
-V.icp <- Vw+(1+1/last)*Vb
-names(beta.icp) <- colnames(V.icp)
-se.icp <- sqrt(diag(V.icp))
-pval.icp <- 2*pnorm(abs(beta.icp/se.icp),lower.tail=F)
+# Fit icp model
+fm.icp <- glm(formula.icp,family="binomial")
+coef.icp <- coef(fm.icp)
+V.icp <- vcov(fm.icp)
 
 # Plot model predictions
 agepred <- c(0:max(age))
@@ -119,7 +80,7 @@ datF <- data.frame(age=agepred,sex=factor("F",levels=c("M","F")))
 XpredM <- model.matrix(xform(formula.icp),datM)
 XpredF <- model.matrix(xform(formula.icp),datF)
 
-beta.sim <- MASS::mvrnorm(1000,beta.icp,V.icp)
+beta.sim <- MASS::mvrnorm(1000,coef.icp,V.icp)
 icpM.sim <- t(apply(beta.sim,1,function(b){expit(as.numeric(XpredM%*%b))}))
 icpF.sim <- t(apply(beta.sim,1,function(b){expit(as.numeric(XpredF%*%b))}))
 icpM <- t(apply(icpM.sim,2,quantile,prob=c(0.025,0.5,0.975)))
@@ -135,9 +96,11 @@ lines(agepred,icpF[,2],col="red",lwd=2)
 legend("topleft",c("Males","Females"),col=c("blue","red"),lwd=2,bty="n")
 dev.off()
 
-# Create imputed datasets with agecat,sex ic status (imputed), los, exit and dead
+# Create imputed datasets with IC status
+M <- 20
+X <- model.matrix(xform(formula.icp),data.frame(age=age,sex=sex))
+beta.sim <- MASS::mvrnorm(M,coef.icp,V.icp)
 imp <- rep(list(data.frame(age=age,sex=sex,ic=ic,los=los,exit=exit,dead=dead)),M)
-beta.sim <- MASS::mvrnorm(M,beta.icp,V.icp)
 for(m in 1:M){
   p <- expit(as.numeric(X[pos2,]%*%beta.sim[m,]))
   imp[[m]]$ic[pos2] <- sapply(p,rbinom,n=1,size=1)
@@ -153,52 +116,47 @@ mean(!is.na(icu_in))
 mean(!is.na(icu_in[sex=="M"]))
 mean(!is.na(icu_in[sex=="F"]))
 
-# Time (for predictions)
-Time <- c(0.001,1:60)
-npred <- length(Time)
-
-# Age categories for mortality plots
-agecat2 <- cut(age,breaks=c(0,70,85,Inf),include.lowest=T)
-for(m in 1:M){imp[[m]] <- cbind(imp[[m]],agecat2)}
-age.mid <- tapply(age,agecat2,mean)
-col.age <- c("green","orange","red")
-
 ###############################################################################
 # TIME TO DEATH MODEL
 
 # Base model
-fm10 <- survreg(Surv(los,dead)~age+ic+sex,data=imp[[1]],dist="weibull"); aic(fm10)
+fm30 <- survreg(Surv(los,dead)~cut(age,breaks=c(0,64,74,104),include.lowest=T)+ic+sex,data=imp[[1]],dist="weibull"); aic(fm30)
+fm31 <- survreg(Surv(los,dead)~cut(age,breaks=c(0,64,74,104),include.lowest=T)*ic+sex,data=imp[[1]],dist="weibull"); aic(fm31)
+fm32 <- survreg(Surv(los,dead)~cut(age,breaks=c(0,64,74,104),include.lowest=T)*ic*sex,data=imp[[1]],dist="weibull"); aic(fm32)
+fm33 <- survreg(Surv(los,dead)~cut(age,breaks=c(0,64,74,104),include.lowest=T),data=imp[[1]],dist="weibull"); aic(fm33)
 
-# Test interaction age*ic using degree 2 FP
-best.FP(fm10,xform=~age,degree=2)$power
-fm11 <- survreg(Surv(los,dead)~FP(age,c(3,3))+ic+sex,data=imp[[1]],dist="weibull"); aic(fm11,2) # worse than fm10
-fm12 <- survreg(Surv(los,dead)~FP(age,c(3,3))*ic+sex,data=imp[[1]],dist="weibull"); aic(fm12,2) # worse than fm10
-
-# Test interaction age*ic using degree 1 FP
-best.FP(fm10,xform=~age,degree=1)$power
-fm13 <- survreg(Surv(los,dead)~FP(age,0.5)+ic+sex,data=imp[[1]],dist="weibull"); aic(fm13,1) # worse than fm10
-fm14 <- survreg(Surv(los,dead)~FP(age,0.5)*ic+sex,data=imp[[1]],dist="weibull"); aic(fm14,1) # worse than fm10
-
-fm15 <- survreg(Surv(los,dead)~FP(age,c(1,1),shift=1,scale=100)*ic+sex,data=imp[[1]],dist="weibull"); aic(fm15)
+# # Test interaction age*ic using degree 2 FP
+# best.FP(fm10,xform=~age,degree=2)$power
+# fm11 <- survreg(Surv(los,dead)~FP(age,c(3,3))+ic+sex,data=imp[[1]],dist="weibull"); aic(fm11,2) # worse than fm10
+# fm12 <- survreg(Surv(los,dead)~FP(age,c(3,3))*ic+sex,data=imp[[1]],dist="weibull"); aic(fm12,2) # worse than fm10
+# 
+# # Test interaction age*ic using degree 1 FP
+# best.FP(fm10,xform=~age,degree=1)$power
+# fm13 <- survreg(Surv(los,dead)~FP(age,0.5)+ic+sex,data=imp[[1]],dist="weibull"); aic(fm13,1) # worse than fm10
+# fm14 <- survreg(Surv(los,dead)~FP(age,0.5)*ic+sex,data=imp[[1]],dist="weibull"); aic(fm14,1) # worse than fm10
+# 
+# fm15 <- survreg(Surv(los,dead)~FP(age,c(1,1),shift=1,scale=100)*ic+sex,data=imp[[1]],dist="weibull"); aic(fm15)
 
 # Best death model
 #****************************************
-formula.dead <- Surv(los,dead)~FP(age,c(3,3),shift=1,scale=100)+ic+sex
+formula.dead <- Surv(los,dead)~cut(age,breaks=c(0,64,74,104),include.lowest=T)
 #****************************************
+col.dead <- c("green","orange","red")
+mid.dead <- tapply(age,cut(age,breaks=c(0,64,74,104),include.lowest=T),mean)
 
 # Fit best model to multiple imputed datasets
-fm1 <- list()
+fm.dead <- list()
 for(m in 1:M){
-  fm1[[m]] <- survreg(formula.dead,data=imp[[m]],dist="weibull")
+  fm.dead[[m]] <- survreg(formula.dead,data=imp[[m]],dist="weibull")
 }
-beta1.tmp <- t(sapply(fm1,function(x){c(coef(x),log(x$scale))})) 
-colnames(beta1.tmp)[ncol(beta1.tmp)] <- "log(scale)"
-beta.dead <- apply(beta1.tmp,2,mean)
-Vw1 <- Reduce("+",lapply(fm1,vcov))/M
-Vb1 <- var(beta1.tmp)
-V.dead <- Vw1+(1+1/M)*Vb1
+cf.tmp <- t(sapply(fm.dead,function(x){c(coef(x),log(x$scale))})) 
+colnames(cf.tmp)[ncol(cf.tmp)] <- "log(scale)"
+coef.dead <- apply(cf.tmp,2,mean)
+Vw <- Reduce("+",lapply(fm.dead,vcov))/M
+Vb <- var(cf.tmp)
+V.dead <- Vw+(1+1/M)*Vb
 se.dead <- sqrt(diag(V.dead))
-pval.dead <- 2*pnorm(abs(beta.dead/se.dead),lower.tail=F)
+pval.dead <- 2*pnorm(abs(coef.dead/se.dead),lower.tail=F)
 
 # Plot survival
 pdf("surv/death.pdf",width=12,height=8)
@@ -208,19 +166,19 @@ for(h in 1:2){ # ic status
     plot(c(0,35),c(0,1),type="n",xlab="Time",ylab="Survival")
     title(paste(c("Without IC","With IC")[h],"-",c("Males","Females")[l]))
     for(m in 1:M){
-      km1 <- survfit(Surv(los,dead)~agecat2,data=subset(imp[[m]],ic==c(0,1)[h] & sex==c("M","F")[l]))
-      lines(km1,col=col.age,lty=3)
+      km <- survfit(Surv(los,dead)~cut(age,breaks=c(0,64,74,104)),data=subset(imp[[m]],ic==c(0,1)[h] & sex==c("M","F")[l]))
+      lines(km,col=col.dead,lty=3)
     }
-    for(k in 1:length(age.mid)){
-      newdat <- data.frame(age=rep(age.mid[k],npred),sex=factor(c("M","F")[l],levels=c("M","F")),ic=c(0,1)[h])
+    for(k in 1:length(mid.dead)){
+      newdat <- data.frame(age=rep(mid.dead[k],npred),sex=factor(c("M","F")[l],levels=c("M","F")),ic=c(0,1)[h])
       X <- model.matrix(xform(formula.dead),newdat)
-      cf <- beta.dead
+      cf <- coef.dead
       mu <- as.numeric(X%*%cf[-length(cf)])
       sigma <- exp(cf[length(cf)])
       surv <- pred.weibull(Time,mu,sigma,what="survival")
-      lines(Time,surv,lwd=2,col=col.age[k])
+      lines(Time,surv,lwd=2,col=col.dead[k])
     }
-    if(h==1 & l==1){legend("bottomleft",levels(agecat2),col=col.age,lwd=1,bty="n")}
+    #if(h==1 & l==1){legend("bottomleft",levels(agecat2),col=col.age,lwd=1,bty="n")}
   }
 }
 dev.off()
@@ -229,41 +187,45 @@ dev.off()
 # TIME TO EXIT
 
 # Base model
-fm20 <- survreg(Surv(los,exit)~age+ic+sex,data=imp[[1]],dist="weibull"); aic(fm20)
+fm40 <- survreg(Surv(los,exit)~cut(age,breaks=c(0,64,74,104),include.lowest=T)+ic+sex,data=imp[[1]],dist="weibull"); aic(fm40)
+fm41 <- survreg(Surv(los,exit)~cut(age,breaks=c(0,64,74,104),include.lowest=T)*ic+sex,data=imp[[1]],dist="weibull"); aic(fm41)
+fm42 <- survreg(Surv(los,exit)~cut(age,breaks=c(0,64,74,104),include.lowest=T)*ic*sex,data=imp[[1]],dist="weibull"); aic(fm42)
 
-# Test interaction age*ic using degree 2 FP
-best.FP(fm20,xform=~age,degree=2)$power
-fm21 <- survreg(Surv(los,exit)~FP(age,c(3,3))+ic+sex,data=imp[[1]],dist="weibull"); aic(fm21,2) # worse than fm20
-fm22 <- survreg(Surv(los,exit)~FP(age,c(3,3))*ic+sex,data=imp[[1]],dist="weibull"); aic(fm22,2) # slightly better than fm20
-
-# Test interaction sex*ic
-fm23 <- survreg(Surv(los,exit)~FP(age,c(3,3))*ic+sex*ic,data=imp[[1]],dist="weibull"); aic(fm23,2) # worse than fm22
-
-# Test interaction sex*age
-fm24 <- survreg(Surv(los,exit)~FP(age,c(3,3))*ic+sex*FP(age,c(1,1)),data=imp[[1]],dist="weibull"); aic(fm24,2) # worse than fm22
-
-fm25 <- survreg(Surv(los,exit)~age*ic+sex,data=imp[[1]],dist="weibull"); aic(fm25,2) # worse than fm22
-best.FP(fm25,xform=~age,degree=2)$power
+# # Test interaction age*ic using degree 2 FP
+# best.FP(fm20,xform=~age,degree=2)$power
+# fm21 <- survreg(Surv(los,exit)~FP(age,c(3,3))+ic+sex,data=imp[[1]],dist="weibull"); aic(fm21,2) # worse than fm20
+# fm22 <- survreg(Surv(los,exit)~FP(age,c(3,3))*ic+sex,data=imp[[1]],dist="weibull"); aic(fm22,2) # slightly better than fm20
+# 
+# # Test interaction sex*ic
+# fm23 <- survreg(Surv(los,exit)~FP(age,c(3,3))*ic+sex*ic,data=imp[[1]],dist="weibull"); aic(fm23,2) # worse than fm22
+# 
+# # Test interaction sex*age
+# fm24 <- survreg(Surv(los,exit)~FP(age,c(3,3))*ic+sex*FP(age,c(1,1)),data=imp[[1]],dist="weibull"); aic(fm24,2) # worse than fm22
+# 
+# fm25 <- survreg(Surv(los,exit)~age*ic+sex,data=imp[[1]],dist="weibull"); aic(fm25,2) # worse than fm22
+# best.FP(fm25,xform=~age,degree=2)$power
 
 # Best exit model
 #*********************************************************************
-formula.exit <- Surv(los,exit)~FP(age,c(1,1),shift=1,scale=100)*ic+sex
-formula.exit <- Surv(los,exit)~FP(age,c(3,3),shift=1,scale=100)*(ic+sex)+ic*sex
+formula.exit <- Surv(los,exit)~cut(age,breaks=c(0,64,74,104),include.lowest=T)*ic*sex
+#formula.exit <- Surv(los,exit)~FP(age,c(3,3),shift=1,scale=100)*(ic+sex)+ic*sex
 #*********************************************************************
+col.exit <- c("green","orange","red")
+mid.exit <- tapply(age,cut(age,breaks=c(0,64,74,104),include.lowest=T),mean)
 
 # Fit best model to multiple imputed datasets
-fm2 <- list()
+fm.exit <- list()
 for(m in 1:M){
-  fm2[[m]] <- survreg(formula.exit,data=imp[[m]],dist="weibull")
+  fm.exit[[m]] <- survreg(formula.exit,data=imp[[m]],dist="weibull")
 }
-beta2.tmp <- t(sapply(fm2,function(x){c(coef(x),log(x$scale))})) 
-colnames(beta2.tmp)[ncol(beta2.tmp)] <- "log(scale)"
-beta.exit <- apply(beta2.tmp,2,mean)
-Vw2 <- Reduce("+",lapply(fm2,vcov))/M
-Vb2 <- var(beta2.tmp)
-V.exit <- Vw2+(1+1/M)*Vb2
+cf.tmp <- t(sapply(fm.exit,function(x){c(coef(x),log(x$scale))})) 
+colnames(cf.tmp)[ncol(cf.tmp)] <- "log(scale)"
+coef.exit <- apply(cf.tmp,2,mean)
+Vw <- Reduce("+",lapply(fm.exit,vcov))/M
+Vb <- var(cf.tmp)
+V.exit <- Vw+(1+1/M)*Vb
 se.exit <- sqrt(diag(V.exit))
-pval.exit <- 2*pnorm(abs(beta.exit/se.exit),lower.tail=F)
+pval.exit <- 2*pnorm(abs(coef.exit/se.exit),lower.tail=F)
 
 # Plot fraction of patients still hospitalized
 pdf("surv/exit.pdf",width=12,height=8)
@@ -273,19 +235,19 @@ for(h in 1:2){ # ic status
     plot(c(0,60),c(0,1),type="n",xlab="Time",ylab="Fraction still hospitalized")
     title(paste(c("Without IC","With IC")[h],"-",c("Males","Females")[l]))
     for(m in 1:M){
-      km1 <- survfit(Surv(los,exit)~agecat2,data=subset(imp[[m]],ic==c(0,1)[h] & sex==c("M","F")[l]))
-      lines(km1,col=col.age,lty=3)
+      km <- survfit(Surv(los,exit)~cut(age,breaks=c(0,64,74,104),include.lowest=T),data=subset(imp[[m]],ic==c(0,1)[h] & sex==c("M","F")[l]))
+      lines(km,col=col.exit,lty=3)
     }
-    for(k in 1:length(age.mid)){
-      newdat <- data.frame(t=Time,age=rep(age.mid[k],npred),sex=factor(c("M","F")[l],levels=c("M","F")),ic=c(0,1)[h])
+    for(k in 1:length(mid.exit)){
+      newdat <- data.frame(age=rep(mid.exit[k],npred),sex=factor(c("M","F")[l],levels=c("M","F")),ic=c(0,1)[h])
       X <- model.matrix(xform(formula.exit),newdat)
-      cf <- beta.exit
+      cf <- coef.exit
       mu <- as.numeric(X%*%cf[-length(cf)])
       sigma <- exp(cf[length(cf)])
       surv <- pred.weibull(Time,mu,sigma,what="survival")
-      lines(Time,surv,lwd=2,col=col.age[k])
+      lines(Time,surv,lwd=2,col=col.exit[k])
     }
-    if(h==1 & l==1){legend("topright",levels(agecat2),col=col.age,lwd=1,bty="n")}
+    #if(h==1 & l==1){legend("topright",levels(agecat2),col=col.age,lwd=1,bty="n")}
   }
 }
 dev.off()
@@ -293,23 +255,29 @@ dev.off()
 ###############################################################################
 # SAVE MORTALITY MODELS FORMULA AND COEFFICIENTS
 mort <- list(
+  lag=list(
+    mlag=fit.lag$mean,
+    vlag=fit.lag$variance
+  ),
   icp=list(
     formula=formula.icp,
-    coef=beta.icp,
+    coef=coef.icp,
     vcov=V.icp
   ),
   dead=list(
     formula=formula.dead,
-    coef=beta.dead,
+    coef=coef.dead,
     vcov=V.dead
   ),
   exit=list(
     formula=formula.exit,
-    coef=beta.exit,
+    coef=coef.exit,
     vcov=V.exit
   )
 )
 save(mort,file="mort.Rdata")
+
+
 
 # Plot two survival curves
 pdf("surv/compare.pdf",width=12,height=8)
